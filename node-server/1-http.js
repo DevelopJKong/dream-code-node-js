a6191e2da1053a592be9756ffeef9ddffb6874a7
DevelopJKong <ybaram40@gmail.com>
2021-12-22 23:34:09 +0900

    9.4 템플릿 엔진 완료

--- node-server/1-http.js
+++ node-server/1-http.js
@@ -2,11 +2,6 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req,res) =>{
-    console.log('incoming...');
-    console.log(req.headers);
-    console.log(req.httpVersion);
-    console.log(req.method);
-    console.log(req.url);
     const url = req.url;    
     res.setHeader('Content-Type','text/html');
     if(url === '/'){
@@ -15,13 +10,14 @@ const server = http.createServer((req,res) =>{
         // res.write("<body><h1>Welcome!</h1></body>")
         // res.write("</html>");
         fs.createReadStream("./html/index.html").pipe(res);
+        console.log(fs.createReadStream("./html/index.html").pipe(res));
         
     } else if( url === '/course') {
         // res.write("<html>");
         // res.write("<head><title>Academy</title></head>");
         // res.write("<body><h1>Course</h1></body>")
         // res.write("</html>");
-        fs.createReadStream("./html/course.html").pipe(res);
+        fs.createReadStream("./html/coures.html").pipe(res);
         
     } else {
         // res.write("<html>");
