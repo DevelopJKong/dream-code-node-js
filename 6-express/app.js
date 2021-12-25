3b9ef7abed3e22844cbfc0c1336695ac1d95adc7
DevelopJKong <ybaram40@gmail.com>
2021-12-25 16:34:29 +0900

    11.5 응답에 대해서 완료

--- 6-express/app.js
+++ 6-express/app.js
@@ -2,8 +2,17 @@ import express from "express"
 const app = express();
 
 
-app.get("/",(req,res,next) => {
-    console.log('get');
-    res.send('hi!');
+app.get("/jeong/:id",(req,res,next) => {
+    //console.log(req.path);
+    //console.log(req.headers);
+    console.log(req.params);
+    console.log(req.params.id);
+    console.log(req.query);
+    console.log(req.query.keyword);
+
+    //res.json({name: 'jeong'});
+    //res.sendStatus(400);
+    res.status(201).send("created");
+    res.setHeader('key','value'); // header 정도도 보낼수있으면? 다운로드도 구현할수있지 않을까?
 })
 app.listen(4020);
